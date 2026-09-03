241f61cb1a14d5c9c9767e0df55aea6885e601d1
Dharanidharanp14 <dharanidharanp@thestackly.com>
2026-09-03 18:20:44 +0530

    due to updation of sdk v57 i have stopped push notification

--- App.jsx
+++ App.jsx
@@ -1,15 +1,41 @@
-import react, { useEffect } from "react";
+import React, { useEffect } from "react";
+import Constants from "expo-constants";
 import AppNavigator from "./src/navigations/AppNavigator";
-import { registerForPushNotificationsAsync } from "./src/services/notificationServices";
 
-function App(){
- useEffect(() => {
-    registerForPushNotificationsAsync();
+function App() {
+  useEffect(() => {
+    const setupNotifications = async () => {
+      if (Constants.executionEnvironment === "storeClient") {
+        console.log(
+          "Expo Go detected - remote push notifications skipped."
+        );
+        return;
+      }
+
+      try {
+        await import(
+          "./src/services/notificationHandler"
+        );
+
+        const {
+          registerForPushNotificationsAsync,
+        } = await import(
+          "./src/services/notificationServices"
+        );
+
+        await registerForPushNotificationsAsync();
+      } catch (error) {
+        console.log(
+          "Notification setup failed:",
+          error
+        );
+      }
+    };
+
+    setupNotifications();
   }, []);
 
-  return(
-    <AppNavigator/>
-  )
+  return <AppNavigator />;
 }
 
-export default App
+export default App;
